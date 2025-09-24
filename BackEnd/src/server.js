9bddd0a75fa41764ffbcb29b87eba0134f05cd83
soham1209 <sohamns1234@gmail.com>
2025-09-24 16:08:58 +0530

    fix: update server listen method to bind to all network interfaces

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -55,7 +55,9 @@ app.use("/api/daily-bookings", dailyBookingRoutes);
 app.use("/api/nutrient-stock", nutrientStockRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  app.listen(PORT, '0.0.0.0', () => console.log(`Server on ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on port ${PORT}`);
+// });
